10ef3973131ff6227b53136424f4d993420b84b6
leooleo <leo.ms097@gmail.com>
2018-11-12 20:18:16 -0200

    Add amazon data set

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sentiment_analysis
 Algorithm to predict a positive or negative sentiment using machine learning
+# Installation
+```
+python3 -m venv venv
+pip install -r requirements.txt
+python3 nltk_setup.py
+```
+
+# Execution
+``` 
+python3 data_mine/amazon/mine.py 
+```
---
+++ data_mine/amazon/mine.py
@@ -0,0 +1,40 @@
+from langdetect import detect
+from datetime import datetime
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+def clean_line(string):
+    ret_string = ""
+    for char in string:        
+        if(char.isalpha() or char.isspace()):            
+            ret_string += char
+
+    ret_string = ' '.join(ret_string.split())
+    return ret_string.lower()
+
+def remove_stopwords(string):
+    tokens = word_tokenize(string)
+    filtered_sentence = [word for word in tokens if word not in stopwords.words("english")]
+    return ' '.join(filtered_sentence) + '\n'
+
+
+myfile   = open("data_mine/amazon/data.txt", "r")
+pos_file = open('data_mine/amazon/p.txt', 'w')
+neg_file = open('data_mine/amazon/n.txt', 'w')
+lines = myfile.readlines()
+
+before = datetime.now()
+for i in range(0,4000):
+    line = lines[i]
+    if(detect(line) == 'en'):
+        if(line.find('__label__1 ') != -1):
+            line = line.replace('__label__1 ','')    
+            clean = remove_stopwords(clean_line(line))
+            neg_file.write(clean)
+
+        elif(line.find('__label__2 ') != -1):
+            line = line.replace('__label__2 ','')    
+            clean = remove_stopwords(clean_line(line))
+            pos_file.write(clean)
+
+print((datetime.now() - before)*100)            
