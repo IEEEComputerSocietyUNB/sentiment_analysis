4a1b0f26521ab0a401ebf8f9e415bf09c190ed16
leooleo <leo.ms097@gmail.com>
2018-11-16 17:32:24 -0200

    Add thread execution(3 threads)

--- data_mine/amazon/mine.py
+++ data_mine/amazon/mine.py
@@ -1,3 +1,4 @@
+from threading import Thread
 from langdetect import detect
 from datetime import datetime
 from nltk.tokenize import word_tokenize
@@ -17,27 +18,59 @@ def remove_stopwords(string):
     filtered_sentence = [word for word in tokens if word not in stopwords.words("english")]
     return ' '.join(filtered_sentence) + '\n'
 
+def mine(lines, thread_range, my_files):    
+    print("Starting process")
+    for i in thread_range:
+        # print("Step", i)
+        line = lines[i]
+        try:
+            if(detect(line) == 'en'):
+                if(line.find('__label__1 ') != -1):
+                    line = line.replace('__label__1 ','')    
+                    clean = remove_stopwords(clean_line(line))
+                    my_files[0].write(clean)
+
+                elif(line.find('__label__2 ') != -1):
+                    line = line.replace('__label__2 ','')    
+                    clean = remove_stopwords(clean_line(line))
+                    my_files[1].write(clean)        
+        except:            
+            continue
+
+    my_files[0].close()
+    my_files[1].close()
+    print("Process finalized")    
+
+
+def tt(lines, thread_range, my_files):
+    for i in thread_range:
+        my_files[0].write(str(i) + '\n')
+        my_files[1].write(str(i) + '\n')
+
+    my_files[0].close()
+    my_files[1].close()
 
 myfile   = open("data_mine/amazon/data.txt", "r")
-pos_file = open('data_mine/amazon/p.txt', 'w')
-neg_file = open('data_mine/amazon/n.txt', 'w')
+pos_file = [open('data_mine/amazon/p0.txt', 'w'), open('data_mine/amazon/p1.txt', 'w'), open('data_mine/amazon/p2.txt', 'w')]
+neg_file = [open('data_mine/amazon/n0.txt', 'w'), open('data_mine/amazon/n1.txt', 'w'), open('data_mine/amazon/n2.txt', 'w')]
 lines = myfile.readlines()
 
 before = datetime.now()
-print("Starting process")
-i = 0
-for line in lines:
-    print("Step", i)    
-    if(detect(line) == 'en'):
-        if(line.find('__label__1 ') != -1):
-            line = line.replace('__label__1 ','')    
-            clean = remove_stopwords(clean_line(line))
-            neg_file.write(clean)
 
-        elif(line.find('__label__2 ') != -1):
-            line = line.replace('__label__2 ','')    
-            clean = remove_stopwords(clean_line(line))
-            pos_file.write(clean)
-    i += 1            
-print("Process finalized")
-print((datetime.now() - before)*100)            
+range_size  = 133333
+upper_range = 133333
+threads = []
+for i in range(0,3):
+    this_range = range(upper_range - range_size, upper_range)
+
+    upper_range += range_size
+    files = [pos_file[i], neg_file[i]]
+    t = Thread(target=mine, args=(lines, this_range,files,))
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
+
+print('All threads returned, took exactly:')
+print((datetime.now() - before))
